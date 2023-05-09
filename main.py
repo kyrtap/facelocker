953b015ff32789c0e9a6e9979afde82f90c6f143
Patryk Brzoza <kyrtap@users.noreply.github.com>
2023-05-09 22:08:15 +0200

    Implement face recognition

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import cv2
+import time
+import sys
+
 from ctypes import CDLL
 
 
@@ -6,9 +10,38 @@ def lock_screen_macos():
     login.SACLockScreenImmediate()
 
 
-def main():
-    lock_screen_macos()
+def face_recognized(cap, face_cascade) -> bool:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    return len(faces) > 0
+
+
+def main(time_interval):
+    cap = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    time_counter = 0
+
+    print('Facelocker started with lock interval {}. Looking for faces...'.format(time_interval))
+
+    while True:
+        if face_recognized(cap, face_cascade):
+            time_counter = 0
+        else:
+            time_counter += 1
+
+        if time_counter == time_interval:
+            lock_screen_macos()
+
+        time.sleep(1)
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        time_interval = sys.argv[1]
+    else:
+        time_interval = 10
+
+    main(time_interval)
