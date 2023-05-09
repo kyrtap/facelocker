588c374373b3a3b14f232ad00e5ff6c792a6cddc
Patryk Brzoza <kyrtap@users.noreply.github.com>
2023-05-09 22:45:24 +0200

    Add argparse parameter handling

--- facelocker.py
+++ facelocker.py
@@ -1,6 +1,6 @@
 import cv2
 import time
-import sys
+import argparse
 
 from ctypes import CDLL
 
@@ -19,29 +19,33 @@ def face_recognized(cap, face_cascade) -> bool:
 
 
 def main(time_interval):
+    print('Starting Facelocker with interval of {} seconds...'.format(time_interval))
+
     cap = cv2.VideoCapture(0)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
     time_counter = 0
 
-    print('Facelocker started with lock interval {}. Looking for faces...'.format(time_interval))
+    print('Looking for faces...'.format(time_interval))
 
     while True:
-        if face_recognized(cap, face_cascade):
-            time_counter = 0
-        else:
-            time_counter += 1
+        try:
+            if face_recognized(cap, face_cascade):
+                time_counter = 0
+            else:
+                time_counter += 1
+        except cv2.error:
+            print('Failed to access camera. Did you grant permissions?')
+            break
 
         if time_counter == time_interval:
             lock_screen_macos()
-
         time.sleep(1)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        time_interval = sys.argv[1]
-    else:
-        time_interval = 10
+    parser = argparse.ArgumentParser()
+    parser.add_argument('interval', nargs='?', default=10, type=int, help='Time interval to lock after')
+    args = parser.parse_args()
 
-    main(time_interval)
+    main(args.interval)
