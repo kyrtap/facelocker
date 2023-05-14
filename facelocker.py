7340217c15e84895ce343c2cd37a57c19e20a5b0
Patryk Brzoza <kyrtap@users.noreply.github.com>
2023-05-14 16:06:52 +0200

    Implement demo mode

--- facelocker.py
+++ facelocker.py
@@ -13,15 +13,22 @@ def lock_screen(platform_name):
         ctypes.windll.user32.LockWorkStation()
 
 
-def face_recognized(cap, face_cascade) -> bool:
+def face_recognized(cap, face_cascade, demo_mode) -> bool:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
+    if demo_mode:
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow('Facelocker', frame)
+        cv2.waitKey(1)
+
     return len(faces) > 0
 
 
-def main(time_interval):
+def main(time_interval=10, demo_mode=False):
     print('Starting Facelocker with interval of {} seconds...'.format(time_interval))
 
     cap = cv2.VideoCapture(0)
@@ -34,7 +41,7 @@ def main(time_interval):
 
     while True:
         try:
-            if face_recognized(cap, face_cascade):
+            if face_recognized(cap, face_cascade, demo_mode):
                 time_counter = 0
             else:
                 time_counter += 1
@@ -50,6 +57,8 @@ def main(time_interval):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('interval', nargs='?', default=10, type=int, help='Time interval to lock after')
+    parser.add_argument('-d', '--demo', action='store_true', default=False,
+                        help='Run in demo mode (show camera frames)')
     args = parser.parse_args()
 
-    main(args.interval)
+    main(args.interval, args.demo)
